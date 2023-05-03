80c53cdcc73126aa230e25ceadb48a92b913fe54
Duan <duw4ng@gmail.com>
2023-05-04 01:04:54 +0800

    menambahkan sticky header & footer

--- src/App.jsx
+++ src/App.jsx
@@ -6,17 +6,17 @@ function App() {
 
   return (
     <>
-    <header className='p-5 text-red-400 border-b'>
+    <header className='p-5 border-b sticky top-0 bg-white'>
       <h1 className='lg:text-4xl text-2xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-pink-400 to-cyan-400 w-fit'>Sewa Kamera Samarinda</h1>
     </header>
-    <div className='grid md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 p-5 gap-2 gap-y-3 h-full grow'>
+    <div className='grid md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 p-5 gap-2 gap-y-3 h-full grow mb-[64px]'>
     {pl.DSLR.map((kamera, index) => {
       return (
         <Pricelist data={kamera} key={index} />
       )
     })}
     </div>
-    <footer className='lg:p-6 text-center p-5 text-slate-200 bg-gradient-to-r from-pink-400 to-cyan-400'>Sewa Kamera Samarinda ©2023</footer>
+    <footer className='lg:p-6 text-center p-5 text-slate-200 bg-gradient-to-r from-pink-400 to-cyan-400 fixed bottom-0 w-full'>Sewa Kamera Samarinda ©2023</footer>
     </>
   )
 }
