979f0a5d61873f2b85c12c260394bcc693f9d60c
Duan <duw4ng@gmail.com>
2023-05-04 02:17:14 +0800

    memperbaiki routing home

--- src/main.jsx
+++ src/main.jsx
@@ -18,6 +18,10 @@ const router = createBrowserRouter([
     errorElement: <ErrorPage />,
     children: [
       {
+        path: "/",
+        element: <Pricelists />,
+      },
+      {
         path: "pricelist",
         element: <Pricelists />
       },
